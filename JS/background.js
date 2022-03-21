65ede1ecc71dfb7cccba82dd79e097e8fe420792
gimhyoungsoo <gimhyoungsoo@gmail.com>
2022-03-22 02:03:06 +0900

    명언과 배경화면 용 이미지 추가[22/03/22]
    Math.floor
    Math.ceil
    Math.round
    document.createElement()
    document.body.appendChild()

---
+++ JS/background.js
@@ -0,0 +1,6 @@
+const  images = ['ecology.png', 'multimedia.png', 'technology.png']
+const chosenImg = images[Math.floor(Math.random() * images.length)]
+
+const imgTag = document.createElement('img')
+imgTag.src = `img/${chosenImg}`
+document.body.appendChild(imgTag)
